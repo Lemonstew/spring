2a7cf788c1001bb1ec959f0925dfa8abadee8905
Lemonstew <colt4949@naver.com>
2024-11-13 09:53:53 +0900

    응답코드와 본문 직접 작성

---
+++ src/main/java/com/example/spring/api/ApiController7.java
@@ -0,0 +1,81 @@
+package com.example.spring.api;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController // Controller + ResponseBody
+@RequestMapping("/api/main7")
+public class ApiController7 {
+
+    @GetMapping("sub1")
+    public String sub1() {
+        return "sub1";
+    }
+
+    @GetMapping("sub3")
+    public ResponseEntity sub3() {
+        ResponseEntity response = new ResponseEntity(HttpStatusCode.valueOf(200));
+        return null;
+    }
+
+    @GetMapping("sub4")
+    public ResponseEntity sub4() {
+        ResponseEntity response = new ResponseEntity(HttpStatusCode.valueOf(404));
+        return null;
+    }
+
+    @GetMapping("sub5")
+    public ResponseEntity sub5() {
+        ResponseEntity response = new ResponseEntity(HttpStatusCode.valueOf(500));
+        return null;
+    }
+
+    @GetMapping("sub6")
+    public ResponseEntity sub6() {
+        ResponseEntity response = ResponseEntity.ok().build();
+        return response;
+    }
+
+    @GetMapping("sub7")
+    public ResponseEntity sub7() {
+        ResponseEntity response = ResponseEntity.notFound().build();
+        return response;
+    }
+
+    @GetMapping("sub8")
+    public ResponseEntity sub8() {
+        ResponseEntity response = ResponseEntity.noContent().build();
+        return response;
+    }
+
+    @GetMapping("sub9")
+    public ResponseEntity sub9() {
+        ResponseEntity response = new ResponseEntity(HttpStatusCode.valueOf(405));
+        return response;
+    }
+
+    @GetMapping("sub10")
+    public ResponseEntity sub10() {
+        ResponseEntity response = ResponseEntity.status(202).build();
+        return response;
+    }
+
+    @GetMapping("sub11")
+    public ResponseEntity sub11() {
+        ResponseEntity response = ResponseEntity.status(403).build();
+        return response;
+    }
+
+    @GetMapping("sub12")
+    public ResponseEntity sub12() {
+        ResponseEntity response
+                = ResponseEntity.status(405)
+                .body(Map.of("name", "son", "age", 30));
+        return response;
+    }
+}
