4eb89fc619e741ef68d075bae673b412939b57f3
Lemonstew <colt4949@naver.com>
2024-11-18 16:54:00 +0900

    11/18

---
+++ src/main/java/com/example/spring/api/ApiController9.java
@@ -0,0 +1,38 @@
+package com.example.spring.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/main9")
+public class ApiController9 {
+    final JwtEncoder jwtEncoder;
+
+    @GetMapping("sub1")
+    public String sub1() {
+
+//        JwtClaimsSet claims = JwtClaimsSet.builder().build();
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self") // 누가
+                .subject("son") // for 누구
+                .issuedAt(Instant.now()) // 언제 생성
+                .expiresAt(Instant.now().plusSeconds(3600)) // 언제까지
+//                .claim("scope", "admin manager user") // 권한
+                .build();
+        // 누구인가. 누가 토큰을 내었는가?
+        // 누가
+        // for 누구
+        // 언제
+        // 언제까지
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+}
